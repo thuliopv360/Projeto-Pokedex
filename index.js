3fcc44540a9ab75a36f7a23e46655a36808faf3f
Thulio Paiva <thuliopv360@gmail.com>
2022-04-27 17:48:16 -0300

    Coloquei o botao deletar

--- index.js
+++ index.js
@@ -95,14 +95,21 @@ app.post("/create", (req, res) => {
     const pokemon = req.body;
     pokemon.id = pokedex.length + 1;
     pokedex.push(pokemon);
-    res.redirect("/");
+    res.redirect("/#cards");
 });
 
 app.get("/detalhes/:id", (req, res) => {
     const id = +req.params.id;
     pokemon = pokedex.find(pokemon => pokemon.id === id);
-    res.redirect("/");
+    res.redirect("/#cadastro");
 });
+
+app.get("/descricao/:id", (req, res) => {
+    const id = +req.params.id;
+    pokemon = pokedex.find(pokemon => pokemon.id === id);
+    res.render("descricao", { pokemon, pokedex });
+});
+
 
 app.post("/update/:id", (req, res) => {
     const id = +req.params.id - 1;
@@ -110,8 +117,14 @@ app.post("/update/:id", (req, res) => {
     newPokemon.id = id + 1;
     pokedex[id] = newPokemon;
     pokemon = undefined;
-    res.redirect("/");
+    res.redirect("/#cards");
 });
+
+app.get("/delete/:id", (req, res) => {
+    const id = +req.params.id - 1;
+    delete pokedex[id];
+    res.redirect("/#cards")
+})
 
 // app.get("/detalhes/:id", (req, res) => {
 //     let pokemon = [];
